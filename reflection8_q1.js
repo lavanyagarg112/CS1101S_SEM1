49cf74f05b92fec0325068a47ab64374105bc7fe
Lavanya Garg <No public email provided>
2023-10-12 07:43:28 +0800

    Changes made from Source Academy

---
+++ reflection8_q1.js
@@ -0,0 +1,34 @@
+function make_withdraw(balance, password) {
+    let count = 0;
+    function withdraw(amount, pass) {
+        if (pass === password && count !== 3){
+            count = 0;
+            if (balance >= amount) {
+                balance = balance - amount;
+                return balance;
+            } else {
+                return "Insufficient funds";
+            }
+            
+        }
+        
+        else {
+            count = count + 1;
+            if (count >= 3){
+                return "Account disabled";
+            }
+            else {
+                return "Wrong password; no withdraw";
+            }
+        }
+        
+    }
+    return withdraw;
+}
+
+const W1 = make_withdraw(100, "my_password");
+W1(40, "his_password");
+W1(40, "his_password");
+W1(40, "his_password");
+W1(40, "my_password");
+
